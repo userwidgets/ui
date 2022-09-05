ec6ac99f40b519ed1483fa8b24fab317843d296e
Linus Johansson <linus@issuefab.com>
2022-09-05 14:31:17 +0200

    organizationpicker work

--- src/components/organizationPicker/index.tsx
+++ src/components/organizationPicker/index.tsx
@@ -1,4 +1,4 @@
-import { Component, h, State } from "@stencil/core"
+import { Component, h, Listen, Prop, State, Watch } from "@stencil/core"
 import { OptionType } from "smoothly"
 import { model } from "../../model"
 import { state } from "../../State"
@@ -9,33 +9,65 @@ import { state } from "../../State"
 	scoped: true,
 })
 export class UserwidgetsOrganizationPicker {
+	// @Prop() location:
 	@State() key?: model.userwidgets.User.Key
+	@State() organizations?: { name: string; value: string }[]
+	@Watch("key")
+	handelKey() {
+		this.key &&
+			(this.organizations = Object.keys(this.key.permissions)
+				.filter(([organizationId]) => organizationId != "*")
+				.map(organizationId => ({ name: organizationId, value: organizationId })))
+		console.log("this.key", this.key)
+		console.log("this.organizations", this.organizations)
+	}
 
 	componentWillLoad() {
 		state.me.listen("key", async key => (this.key = await key))
+		console.log("key ", this.key)
 		console.log("organization picker componentWillLoad")
 	}
+
+	@Listen("menuClose")
 	handleMenuClose(event: CustomEvent<OptionType[]>) {
 		event.stopPropagation()
-		state.options.organizationId = event.detail[0].value // not pretty but the length of event.detail should = 1.because picker multiple=false
+		console.log("handleMenuClose state", state.options.organizationId)
+		state.options = { organizationId: event.detail[0].value } // not pretty but the length of event.detail should = 1 because picker multiple=false
 		window.location.href = window.origin // you only get one chance
 	}
+
 	render() {
-		let result: any
-		if (this.key) {
-			const organizations = Object.keys(this.key.permissions)
-				.filter(([organizationId]) => organizationId != "*")
-				.map(organizationId => ({ name: organizationId, value: organizationId }))
+		return this.organizations ? (
+			<div>
+				<smoothly-picker label="Organization" multiple={false} options={this.organizations}></smoothly-picker>
+				<p>
+					Choose <a href={window.origin}> organization</a>{" "}
+					{/* this wont be necessary. The table should rerender with correct data immediately*/}
+				</p>
+			</div>
+		) : (
+			<div>
+				<p>You are not a member of any organization.</p>
+			</div>
+		)
+		// if (this.key) {
+		// 	const organizations = Object.keys(this.key.permissions)
+		// 		.filter(([organizationId]) => organizationId != "*")
+		// 		.map(organizationId => ({ name: organizationId, value: organizationId }))
 
-			state.options.organizationId = organizations[0].value
-			result =
-				organizations.length != 0 ? null : (
-					<smoothly-picker label="Organization" multiple={false} options={organizations}></smoothly-picker>
-					// probably going to need a submit button to actually close the window
-				)
-		} else
-			result = "testing"
+		// 	state.options.organizationId = organizations[0].value
+		// 	result =
+		// 		organizations.length != 0 ? null : (
+		// 			<div>
+		// 				<smoothly-picker label="Organization" multiple={false} options={organizations}></smoothly-picker>
+		// 				<p>
+		// 					Choose <a href={window.origin}> organization</a>
+		// 				</p>
+		// 			</div>
+		// 		)
+		// } else
+		// 	result = "testing"
 
-		return result
+		// return result
 	}
 }
