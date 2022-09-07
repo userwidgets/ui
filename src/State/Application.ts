4e91488c10d2d4b301ceff6e0a99ac6228b335d9
Linus Johansson <linus@issuefab.com>
2022-09-07 10:15:23 +0200

    application state implemented

--- src/State/Application.ts
+++ src/State/Application.ts
@@ -1,1 +1,36 @@
+import * as  gracely from "gracely"
+import { Client } from "../Client"
+import { model } from "../model"
+import { Listenable } from "./Listenable"
+import { Options } from "./Options"
 
+export class Application {
+	#options: Options = {}
+	set options(options: Options) {
+		options.applicationId != this.#options?.applicationId && (this.#application = undefined)
+		this.#options = options
+	}
+	#application?: Promise<model.userwidgets.Application | undefined>
+	get application() {
+		return (
+			this.#application ??
+			(this.#self.application = this.#client.application
+				.fetch()
+				.then(response => (gracely.Error.is(response) ? undefined : response)))
+		)
+	}
+
+	set application(application: Promise<model.userwidgets.Application | undefined>) {
+		this.#application = application
+	}
+	#client: Client
+	#self: Application & Listenable<Application>
+	constructor(listenable: Application & Listenable<Application>, client: Client) {
+		this.#client = client
+		this.#self = listenable
+	}
+	static create(client: Client): Application & Listenable<Application> {
+		const listenable = new Listenable<Application>() as Application & Listenable<Application>
+		return Listenable.load(new this(listenable, client), listenable)
+	}
+}
--- src/State/index.ts
+++ src/State/index.ts
@@ -1,11 +1,11 @@
 import { Client } from "../Client"
 import { client } from "../client"
+import { Application } from "./Application"
 import { Listenable } from "./Listenable"
 import { Me } from "./Me"
 import { Options } from "./Options"
 import { Users } from "./Users"
 import { Version } from "./Version"
-
 export class State {
 	#options: Options = {}
 	set options(options: Options) {
@@ -16,17 +16,19 @@ export class State {
 	get options() {
 		return this.#options
 	}
+	set onUnauthorized(value: () => Promise<boolean>) {
+		client.onUnauthorized = value
+	}
 	readonly me: Listenable<Me> & Me
 	readonly users: Listenable<Users> & Users
+	readonly application: Listenable<Application> & Application
 	readonly version: Version
 	constructor(client: Client) {
 		this.me = Me.create(client)
 		this.users = Users.create(client)
+		this.application = Application.create(client)
 		this.version = new Version(client)
 		this.me.listen("options", options => (this.#options = options) && (this.users.options = this.#options))
-	}
-	set onUnauthorized(value: () => Promise<boolean>) {
-		client.onUnauthorized = value
 	}
 }
 
