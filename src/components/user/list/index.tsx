70693a6ca9edafee9fed69635623be54e64381c3
Elias Eriksson <eliaseriksson95@gmail.com>
2022-11-17 17:35:30 +0100

    removing self

--- src/components/user/list/index.tsx
+++ src/components/user/list/index.tsx
@@ -42,25 +42,27 @@ export class UserwidgetsUserList {
 					<smoothly-table-header>Email</smoothly-table-header>
 					<smoothly-table-header></smoothly-table-header>
 				</smoothly-table-row>
-				{this.users?.map(user => (
-					/*user.email == this.key?.email ? null :*/ <smoothly-table-expandable-row>
-						<smoothly-table-cell>{[user.name.first, user.name.last].join(" ")}</smoothly-table-cell>
-						<smoothly-table-cell>{user.email}</smoothly-table-cell>
-						<div class={"detail"} slot="detail">
-							<slot name={user.email}></slot>
-							<div class={"table"}>
-								<userwidgets-user-status state={this.state} user={user}></userwidgets-user-status>
-								<userwidgets-user-permissions-update
-									state={this.state}
-									user={user}></userwidgets-user-permissions-update>
-								<userwidgets-organization-user-remove
-									state={this.state}
-									user={user}
-									class={"right"}></userwidgets-organization-user-remove>
+				{this.users?.map(user =>
+					user.email == this.key?.email ? null : (
+						<smoothly-table-expandable-row>
+							<smoothly-table-cell>{[user.name.first, user.name.last].join(" ")}</smoothly-table-cell>
+							<smoothly-table-cell>{user.email}</smoothly-table-cell>
+							<div class={"detail"} slot="detail">
+								<slot name={user.email}></slot>
+								<div class={"table"}>
+									<userwidgets-user-status state={this.state} user={user}></userwidgets-user-status>
+									<userwidgets-user-permissions-update
+										state={this.state}
+										user={user}></userwidgets-user-permissions-update>
+									<userwidgets-organization-user-remove
+										state={this.state}
+										user={user}
+										class={"right"}></userwidgets-organization-user-remove>
+								</div>
 							</div>
-						</div>
-					</smoothly-table-expandable-row>
-				))}
+						</smoothly-table-expandable-row>
+					)
+				)}
 			</smoothly-table>
 		)
 	}
