3ac14465796d646be8d689b06f80375b5584944a
Linus Johansson <linus@issuefab.com>
2022-11-29 16:27:15 +0100

    change-password swedish translation implemented

--- src/components/user/change-password/index.tsx
+++ src/components/user/change-password/index.tsx
@@ -1,9 +1,10 @@
 import { Component, Event, EventEmitter, h, Listen, Prop, State } from "@stencil/core"
 import * as gracely from "gracely"
+import * as langly from "langly"
 import { Notice } from "smoothly"
 import { client } from "../../../client"
 import { model } from "../../../model"
-
+import * as translation from "./translation"
 @Component({
 	tag: "userwidgets-change-password",
 	styleUrl: "style.css",
@@ -13,11 +14,13 @@ export class ChangePassword {
 	@State() key?: model.userwidgets.User.Key
 	@Event() notice: EventEmitter<Notice>
 	@Prop() state: model.State
+	@State() t: langly.Translate
 	async componentWillLoad(): Promise<void> {
 		this.state.me.listen("key", async promise => {
 			const key = await promise
 			this.key = key ? key : undefined
 		})
+		this.state.listen("language", language => (this.t = translation.create(language)))
 	}
 	@Listen("submit")
 	async handleSubmit(event: CustomEvent<{ old: string; new: string; repeat: string }>) {
@@ -25,9 +28,9 @@ export class ChangePassword {
 		event.stopPropagation()
 		const passwords = Object.fromEntries(new FormData(event.target as HTMLFormElement))
 		if (!model.userwidgets.User.Password.Change.is(passwords))
-			this.notice.emit(Notice.failed("Missing fields."))
+			this.notice.emit(Notice.failed(this.t("Missing fields.")))
 		else if (passwords.new != passwords.repeat)
-			this.notice.emit(Notice.failed("New password was not repeated correctly."))
+			this.notice.emit(Notice.failed(this.t("New password was not repeated correctly.")))
 		else {
 			const key = await client.fullKey
 			if (key) {
@@ -43,17 +46,17 @@ export class ChangePassword {
 	render() {
 		return (
 			<form>
-				Change password for user <code>{this.key?.email}</code>
+				{this.t("Change password for user ")}<code>{this.key?.email}</code>
 				<smoothly-input name="old" type="password">
-					Old password
+					{this.t("Old password")}
 				</smoothly-input>
 				<smoothly-input name="new" type="password">
-					New password
+					{this.t("New password")}
 				</smoothly-input>
 				<smoothly-input name="repeat" type="password">
-					Repeat password
+					{this.t("Repeat password")}
 				</smoothly-input>
-				<smoothly-submit>Change password</smoothly-submit>
+				<smoothly-submit>{this.t("Change password")}</smoothly-submit>
 			</form>
 		)
 	}
