90326969d860d237396f6a64ebdbe2072eaa7890
Elias Eriksson <eliaseriksson95@gmail.com>
2023-04-06 13:33:37 +0200

    added missing function to state org

--- src/State/Organizations.ts
+++ src/State/Organizations.ts
@@ -4,6 +4,7 @@ import { Client } from "../Client"
 import { model } from "../model"
 import { Base } from "./Base"
 import { Options } from "./Options"
+import { Users } from "./Users"
 
 export class Organizations extends Base<Organizations, Client> {
 	#options: Organizations["options"] = {}
@@ -36,6 +37,9 @@ export class Organizations extends Base<Organizations, Client> {
 	set current(current: Organizations["current"]) {
 		this.#current = current
 	}
+	constructor(client: Client, private users: WithListenable<Users>) {
+		super(client)
+	}
 	fetch(options?: Organizations["options"]): Organizations["value"] {
 		options = options ?? this.options
 		return (this.listenable.value = !options?.organization
@@ -44,8 +48,23 @@ export class Organizations extends Base<Organizations, Client> {
 					.fetch(options.organization)
 					.then(response => (!isOrganizations(response) ? false : response)))
 	}
-	static create(client: Client, options: WithListenable<Options>): WithListenable<Organizations> {
-		const backend = new this(client)
+	async removeUser(email: string): Promise<userwidgets.Organization | false> {
+		const promise = !this.options.organization
+			? Promise.resolve(false as const)
+			: this.client.organization
+					.removeUser(this.options.organization, email)
+					.then(response => (!userwidgets.Organization.is(response) ? false : response))
+		const result = await promise
+		if (result)
+			this.users.fetch()
+		return result
+	}
+	static create(
+		client: Client,
+		users: WithListenable<Users>,
+		options: WithListenable<Options>
+	): WithListenable<Organizations> {
+		const backend = new this(client, users)
 		const listenable = Listenable.load(backend)
 		options.listen("value", options => (backend.options = options))
 		return listenable
--- src/State/index.ts
+++ src/State/index.ts
@@ -6,13 +6,15 @@ import { Locales } from "./Locales"
 import { Me } from "./Me"
 import { Options } from "./Options"
 import { Organizations } from "./Organizations"
+import { Users } from "./Users"
 
 export class State extends Base<State, Client> {
 	readonly locales = Locales.create()
 	readonly me = Me.create(this.client)
+	readonly users = Users.create(this.client)
 	readonly options = Options.create(this.me)
 	readonly applications = Applications.create(this.client, this.options)
-	readonly organizations = Organizations.create(this.client, this.options)
+	readonly organizations = Organizations.create(this.client, this.users, this.options)
 	static create(client: Client): WithListenable<State> {
 		return Listenable.load(new this(client))
 	}
