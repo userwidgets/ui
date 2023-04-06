dc5c4e485caaa022bc03f6bbbbd8cf83b5e4072d
toli2003 <toli2003@student.miun.se>
2023-04-06 13:56:05 +0200

    user logout red fix

--- src/components/user/logout/index.tsx
+++ src/components/user/logout/index.tsx
@@ -1,5 +1,6 @@
 import { Component, h, Prop, State } from "@stencil/core"
 import { redirect } from "smoothly"
+import { userwidgets } from "@userwidgets/model"
 import { model } from "../../../model"
 
 @Component({
@@ -9,7 +10,7 @@ import { model } from "../../../model"
 })
 export class Logout {
 	@Prop() state: model.State
-	@State() key?: model.userwidgets.User.Key
+	@State() key?: userwidgets.User.Key
 	componentWillLoad() {
 		this.state.me.listen("key", async promise => {
 			const key = await promise
