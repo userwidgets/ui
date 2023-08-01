59f567daa51fe0d2e0e8159e3cd938893dddfb4c
Linus Johansson <linus@issuefab.com>
2023-08-01 14:17:15 +0200

    first draft of table

--- src/components/organization/list/index.tsx
+++ src/components/organization/list/index.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, State } from "@stencil/core"
+import { Component, h, Prop, State } from "@stencil/core"
 import { userwidgets } from "@userwidgets/model"
 import { model } from "../../../model"
 
@@ -11,4 +11,25 @@ export class userwidgetsOrganizationList {
 	@Prop() state: model.State
 	@State() organizations?: userwidgets.Organization[]
 	@State() key?: userwidgets.User.Key
+	componentWillLoad() {
+		this.state.me.listen("key", async key => (this.key = key || undefined))
+		this.state.organizations.listen("value", organizations => (this.organizations = organizations || undefined))
+	}
+
+	render() {
+		return (
+			<smoothly-table>
+				<smoothly-table-row>
+					<smoothly-table-header>Name</smoothly-table-header>
+					<smoothly-table-header>id</smoothly-table-header>
+				</smoothly-table-row>
+				{this.organizations?.map(organization => (
+					<smoothly-table-expandable-row>
+						<smoothly-table-cell>{organization.name}</smoothly-table-cell>
+						<smoothly-table-cell>{organization.id}</smoothly-table-cell>
+					</smoothly-table-expandable-row>
+				))}
+			</smoothly-table>
+		)
+	}
 }
