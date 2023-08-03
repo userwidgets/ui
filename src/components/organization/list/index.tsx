61e55f397098fc7af67e19d668c99f6fb97c2937
Linus Johansson <linus@issuefab.com>
2023-08-03 09:39:13 +0200

    updating the name of org works.

--- src/components/organization/list/index.tsx
+++ src/components/organization/list/index.tsx
@@ -11,23 +11,68 @@ export class userwidgetsOrganizationList {
 	@Prop() state: model.State
 	@State() organizations?: userwidgets.Organization[]
 	@State() key?: userwidgets.User.Key
+	@State() editing = false
 	componentWillLoad() {
 		this.state.me.listen("key", async key => (this.key = key || undefined))
 		this.state.organizations.listen("value", organizations => (this.organizations = organizations || undefined))
 	}
 
+	// handle the submit event
+	async submitHandler(event: CustomEvent<model.Data>, organization: userwidgets.Organization) {
+		event.preventDefault()
+		event.stopPropagation()
+		//console.log("submitHandler", event.detail.name, organization)
+		const newName = {
+			name: event.detail.name,
+		}
+		userwidgets.Organization.Changeable.type.is(newName) &&
+			(await this.state.organizations.update(organization.id, newName)) &&
+			console.log("i think this worked")
+	}
 	render() {
 		return (
 			<smoothly-table>
 				<smoothly-table-row>
 					<smoothly-table-header>Name</smoothly-table-header>
-					<smoothly-table-header>id</smoothly-table-header>
+					<smoothly-table-header>Modified</smoothly-table-header>
 				</smoothly-table-row>
 				{this.organizations?.map(organization => (
 					<smoothly-table-expandable-row>
 						<smoothly-table-cell>{organization.name}</smoothly-table-cell>
-						<smoothly-table-cell>{organization.id}</smoothly-table-cell>
+						<smoothly-table-cell>{organization.modified}</smoothly-table-cell>
+						<div class={"detail"} slot="detail">
+							<smoothly-form
+								looks={"grid"}
+								onSmoothlyFormSubmit={e =>
+									this.submitHandler(e, organization)
+								} /* consider showing the id, created, and modified if user is superAdmin(?)*/
+							>
+								<smoothly-input
+									disabled={false}
+									name="name" /* the organization name should be editable, its going to require api calls that elias created. */
+								>
+									{organization.name}
+								</smoothly-input>
+								{/* <smoothly-input disabled={true} name="id">
+									{organization.id}
+								</smoothly-input> */}
+								<smoothly-submit slot="submit">Update Organization name</smoothly-submit>
+							</smoothly-form>
+
+							<smoothly-table /* Members should be its own component (userwidgets-user-list) 
+							that can be used both here and on its own. Make it toggle between showing and not showing.
+							look into how issuefab does the toggle to show some of the nested tables in detail-slot*/
+							>
+								<smoothly-table-row>
+									<smoothly-table-header>Members</smoothly-table-header>
+								</smoothly-table-row>
+								{organization.users.map(user => (
+									<smoothly-table-row>{user}</smoothly-table-row>
+								))}
+							</smoothly-table>
+						</div>
 					</smoothly-table-expandable-row>
+					// We are going to want to include a component to view and edit an organizations permissions
 				))}
 			</smoothly-table>
 		)
--- src/components/user/list/member/index.tsx
+++ src/components/user/list/member/index.tsx
@@ -10,16 +10,20 @@ import * as translation from "./translation"
 	scoped: true,
 })
 export class UserwidgetsUserListMember {
+	// If its given an organization then it lists the members and invites of that organization.
+	// Otherwise it shows the organization the user is currently logged on to.
+	// allows editing of users, resending invites, and removing users. (pending the permissions ofc)
 	@Prop() state: model.State
 	@State() users?: userwidgets.User.Readable[]
 	@State() key?: userwidgets.User.Key
-	@State() organization?: userwidgets.Organization
+	@Prop({ mutable: true }) organization?: userwidgets.Organization
 	@State() translate: langly.Translate = translation.create("en")
 	componentWillLoad() {
 		this.state.users.listen("value", users => (this.users = users || undefined))
 		this.state.me.listen("key", async key => (this.key = key || undefined))
 		this.state.locales.listen("language", language => (this.translate = translation.create(language)))
-		this.state.organizations.listen("current", organization => (this.organization = organization || undefined))
+		this.organization ??
+			this.state.organizations.listen("current", organization => (this.organization = organization || undefined))
 	}
 	render() {
 		return (
@@ -39,10 +43,10 @@ export class UserwidgetsUserListMember {
 										<slot name={user.email}></slot>
 										<div class={"userwidgets-detail"}>
 											<userwidgets-user-permissions-update state={this.state} user={user}>
-												<smoothly-icon name="paper-plane-sharp" size="small"></smoothly-icon>
+												<smoothly-icon name="paper-plane-sharp" size="tiny"></smoothly-icon>
 											</userwidgets-user-permissions-update>
 											<userwidgets-organization-user-remove state={this.state} user={user} class={"right"}>
-												<smoothly-icon name="person-remove-sharp" size="small"></smoothly-icon>
+												<smoothly-icon name="person-remove-sharp" size="tiny"></smoothly-icon>
 											</userwidgets-organization-user-remove>
 										</div>
 									</div>
