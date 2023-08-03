310cf9bb16262f80a6e5be475feecbe7ae36486e
Linus Johansson <linus@issuefab.com>
2023-08-03 11:13:20 +0200

    created files for edit-button

---
+++ src/components/edit-button/index.tsx
@@ -0,0 +1,41 @@
+import { Component, Event, EventEmitter, h, Host, Prop } from "@stencil/core"
+
+@Component({
+	tag: "userwidgets-edit-button",
+	styleUrl: "style.css",
+	scoped: true,
+})
+export class UserwidgetsEditButton {
+	@Prop({ mutable: true }) change = false
+	@Prop() disabled = false
+	@Event() userwidgetsEditStart: EventEmitter<void>
+	@Event() userwidgetsEditEnd: EventEmitter<void>
+
+	handleClick() {
+		!this.change ? this.userwidgetsEditStart.emit() : this.userwidgetsEditEnd.emit()
+		this.change = !this.change
+	}
+	render() {
+		return (
+			<Host class="edit button">
+				{this.change ? (
+					<smoothly-submit disabled={this.disabled} size="flexible">
+						<smoothly-icon size="small" color="success" name="checkmark-outline" toolTip={"Submit changes"} />
+					</smoothly-submit>
+				) : null}
+				<smoothly-button
+					size={"flexible"}
+					fill={!this.change ? "outline" : "default"}
+					color={!this.change ? "primary" : "warning"}
+					onClick={() => this.handleClick()}>
+					<smoothly-icon
+						size="small"
+						fill={!this.change ? "outline" : "default"}
+						name={!this.change ? "pencil" : "close"}
+						toolTip={this.change ? "Stop editing and revert changes" : "Start editing"}
+					/>
+				</smoothly-button>
+			</Host>
+		)
+	}
+}
