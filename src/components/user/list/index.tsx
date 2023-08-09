a471a41d2e02675ae58dda7f6141af2e14768a7f
Linus Johansson <linus@issuefab.com>
2023-08-09 13:17:08 +0200

    user-list

--- src/components/user/list/index.tsx
+++ src/components/user/list/index.tsx
@@ -23,17 +23,25 @@ export class UserwidgetsUserList {
 			this.state.organizations.listen("current", organization => (this.organization = organization || undefined))
 	}
 	render() {
-		return (
+		return [
+			<smoothly-summary color="primary" size="small" fill="outline">
+				<span slot="summary">Invites</span>
+				<userwidgets-user-list-invited slot="content" state={this.state} />
+			</smoothly-summary>,
 			<smoothly-table>
 				<smoothly-table-row>
-					<smoothly-table-header>{this.translate("Name")}</smoothly-table-header>
+					<smoothly-table-header>{this.translate("Members")}</smoothly-table-header>
 					<smoothly-table-header>{this.translate("Email")}</smoothly-table-header>
 					<smoothly-table-header></smoothly-table-header>
 				</smoothly-table-row>
 				{this.users?.map(user => (
-					<userwidgets-user user={user} state={this.state} />
+					<smoothly-table-expandable-row>
+						<smoothly-table-cell>{user.name.first + " " + user.name.last}</smoothly-table-cell>
+						<smoothly-table-cell>{user.email}</smoothly-table-cell>
+						<userwidgets-user slot="detail" user={user} state={this.state} />
+					</smoothly-table-expandable-row>
 				))}
-			</smoothly-table>
-		)
+			</smoothly-table>,
+		]
 	}
 }
