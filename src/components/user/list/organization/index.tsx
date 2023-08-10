1c7270772031cbd08f3964d55ba0f430442be829
Linus Johansson <linus@issuefab.com>
2023-08-10 09:31:23 +0200

    user-list-organization component

---
+++ src/components/user/list/organization/index.tsx
@@ -0,0 +1,17 @@
+import { Component, h, Prop, State } from "@stencil/core"
+import { userwidgets } from "@userwidgets/model"
+import { model } from "../../../../model"
+
+@Component({
+	tag: "userwidgets-user-list-organization",
+})
+export class UserwidgetsUserListOrganization {
+	@Prop() state: model.State
+	@State() organization?: userwidgets.Organization
+	componentWillLoad() {
+		this.state.organizations.listen("current", organization => (this.organization = organization || undefined))
+	}
+	render() {
+		return <userwidgets-user-list state={this.state} organization={this.organization} />
+	}
+}
