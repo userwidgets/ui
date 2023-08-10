2962f8bad1922151f2cac234c8b4703d617cad3d
Linus Johansson <linus@issuefab.com>
2023-08-10 10:48:55 +0200

    user-list filters the members per org

--- src/components/user/list/index.tsx
+++ src/components/user/list/index.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop, State } from "@stencil/core"
+import { Component, h, Host, Prop, State } from "@stencil/core"
 import * as langly from "langly"
 import { userwidgets } from "@userwidgets/model"
 import { model } from "../../../model"
@@ -19,29 +19,34 @@ export class UserwidgetsUserList {
 		this.state.users.listen("value", users => (this.users = users || undefined))
 		this.state.me.listen("key", async key => (this.key = key || undefined))
 		this.state.locales.listen("language", language => (this.translate = translation.create(language)))
-		this.organization ??
-			this.state.organizations.listen("current", organization => (this.organization = organization || undefined))
 	}
 	render() {
-		return [
-			<smoothly-summary color="primary" size="small" fill="outline">
-				<span slot="summary">Invites</span>
-				<userwidgets-user-list-invited slot="content" state={this.state} />
-			</smoothly-summary>,
-			<smoothly-table>
-				<smoothly-table-row>
-					<smoothly-table-header>{this.translate("Members")}</smoothly-table-header>
-					<smoothly-table-header>{this.translate("Email")}</smoothly-table-header>
-					<smoothly-table-header></smoothly-table-header>
-				</smoothly-table-row>
-				{this.users?.map(user => (
-					<smoothly-table-expandable-row>
-						<smoothly-table-cell>{user.name.first + " " + user.name.last}</smoothly-table-cell>
-						<smoothly-table-cell>{user.email}</smoothly-table-cell>
-						<userwidgets-user slot="detail" user={user} state={this.state} />
-					</smoothly-table-expandable-row>
-				))}
-			</smoothly-table>,
-		]
+		const users = this.organization
+			? this.users?.filter(user => this.organization?.users.includes(user.email))
+			: this.users
+		return (
+			<Host>
+				<smoothly-summary color="primary" size="small" fill="outline">
+					<span slot="summary">Invites</span>
+					<userwidgets-user-list-invited slot="content" state={this.state} />
+				</smoothly-summary>
+				<smoothly-table>
+					<smoothly-table-row>
+						<smoothly-table-header>{this.translate("Members")}</smoothly-table-header>
+						<smoothly-table-header>{this.translate("Email")}</smoothly-table-header>
+						<smoothly-table-header></smoothly-table-header>
+					</smoothly-table-row>
+					{users
+						? users.map(user => (
+								<smoothly-table-expandable-row>
+									<smoothly-table-cell>{user.name.first + " " + user.name.last}</smoothly-table-cell>
+									<smoothly-table-cell>{user.email}</smoothly-table-cell>
+									<userwidgets-user slot="detail" user={user} state={this.state} />
+								</smoothly-table-expandable-row>
+						  ))
+						: null}
+				</smoothly-table>
+			</Host>
+		)
 	}
 }
