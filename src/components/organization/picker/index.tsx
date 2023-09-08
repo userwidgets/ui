735cc28d7c09f0b4538b2c5cd49e99274a70b26c
Linus Johansson <linus@issuefab.com>
2023-09-08 11:55:12 +0200

    remove comments

--- src/components/organization/picker/index.tsx
+++ src/components/organization/picker/index.tsx
@@ -10,20 +10,16 @@ import * as translation from "./translation"
 })
 export class UserwidgetsOrganizationPicker {
 	@Prop() state: model.State
-	@State() key?: userwidgets.User.Key
 	@State() organizations?: userwidgets.Organization[]
 	@State() organization?: userwidgets.Organization
 	@State() translate: langly.Translate = translation.create("en")
 
 	componentWillLoad() {
-		this.state.me.listen("key", key => (this.key = key || undefined)) //do i need this?
-		this.state.organizations.listen("value", organizations => (this.organizations = organizations || undefined)) //do i need this?
-		this.state.organizations.listen("current", organization => (this.organization = organization || undefined)) //do i need this?
+		this.state.organizations.listen("value", organizations => (this.organizations = organizations || undefined))
+		this.state.organizations.listen("current", organization => (this.organization = organization || undefined))
 		this.state.locales.listen("language", language => (this.translate = translation.create(language)))
 	}
-	// clickHandler() {}
 	inputHandler(event: CustomEvent<Record<string, unknown>>) {
-		// the input handler should update the active organization
 		event.stopPropagation()
 		const organization = event.detail.organization
 		if (userwidgets.Organization.is(organization))
@@ -33,20 +29,13 @@ export class UserwidgetsOrganizationPicker {
 		return (
 			<Host>
 				<smoothly-form>
-					<smoothly-picker
-						name="organization"
-						// onClick={() => this.clickHandler()} // we are probably going to have to do something on click ?
-						onSmoothlyInput={e => this.inputHandler(e)}>
+					<smoothly-picker name="organization" onSmoothlyInput={e => this.inputHandler(e)}>
 						<span slot="search">Search</span>
-						{this.organizations?.map(
-							(
-								organization // i think this is correct
-							) => (
-								<smoothly-picker-option selected={organization == this.organization} value={organization}>
-									{organization.name}
-								</smoothly-picker-option>
-							)
-						)}
+						{this.organizations?.map(organization => (
+							<smoothly-picker-option selected={organization == this.organization} value={organization}>
+								{organization.name}
+							</smoothly-picker-option>
+						))}
 					</smoothly-picker>
 				</smoothly-form>
 			</Host>
