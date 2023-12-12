5b98bf96e5d2cebaa59ca584793180e8fbdf7e7c
Elias Eriksson <elias@issuefab.com>
2023-12-12 11:40:25 +0100

    fix: stopped infinite loop (#107)
    * fix: stopped infinite loop
    
    * current set to false if value is empty array

--- src/State/Organizations.ts
+++ src/State/Organizations.ts
@@ -29,8 +29,8 @@ export class Organizations extends smoothly.StateBase<Organizations, userwidgets
 			if (value != this.#current)
 				this.listenable.current = value
 		} else if (!this.#current) {
-			if (value && value.length)
-				this.listenable.current = value.at(0)
+			if (value)
+				this.listenable.current = value.at(0) ?? false
 		} else {
 			const id = this.#current.id
 			const index = value.findIndex(organization => organization.id == id)
@@ -68,9 +68,9 @@ export class Organizations extends smoothly.StateBase<Organizations, userwidgets
 					.list()
 					.then(response => (Response.fetch.is(response) ? response : false)))
 		const result = await promise
-		this.request = undefined
 		if (this.#value != result)
 			this.listenable.value = result
+		this.request = undefined
 		return result || false
 	}
 
