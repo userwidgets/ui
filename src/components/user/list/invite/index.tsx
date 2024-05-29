90ad4c0381158e4ff1b6997777b4778a25bd4df4
Elias Eriksson <eliaseriksson95@gmail.com>
2024-05-29 13:10:35 +0200

    invite list builds

--- src/components/user/list/invite/index.tsx
+++ src/components/user/list/invite/index.tsx
@@ -1,46 +1,43 @@
-import { Component, Event, EventEmitter, h, Host, Prop, State } from "@stencil/core"
+import { Component, ComponentWillLoad, Event, EventEmitter, h, Host, Prop, State, VNode } from "@stencil/core"
 import { langly } from "langly"
 import { smoothly } from "smoothly"
+import { SmoothlyFormCustomEvent } from "smoothly/dist/types/components"
 import { userwidgets } from "@userwidgets/model"
 import { model } from "../../../../model"
 import * as translation from "./translation"
+
+// TODO update to use new forms
 @Component({
 	tag: "userwidgets-user-list-invite",
 	styleUrl: "style.css",
 	scoped: true,
 })
-export class UserwidgetsUserListInvite {
+export class UserwidgetsUserListInvite implements ComponentWillLoad {
 	private form?: HTMLSmoothlyFormElement
 	@Prop() state: model.State
 	@Prop({ mutable: true }) organization?: userwidgets.Organization | null = null
 	@State() invite?: Partial<userwidgets.Organization.Changeable.Invite> = {}
 	@State() translate: langly.Translate = translation.create("en")
-	@State() disabled = false
 	@Event() userwidgetsFormControls: EventEmitter<{ clear: () => void }>
 	@Event() notice: EventEmitter<smoothly.Notice>
 
-	componentWillLoad() {
+	componentWillLoad(): void {
 		this.userwidgetsFormControls.emit({ clear: () => this.form?.clear() })
 		if (this.organization === null)
 			this.state.organizations.listen("current", organization => (this.organization = organization || undefined))
 	}
 
-	inputHandler(event: CustomEvent<smoothly.Data>) {
+	async submitHandler(event: SmoothlyFormCustomEvent<smoothly.Submit>) {
 		event.stopPropagation()
-	}
-
-	async submitHandler(event: CustomEvent<smoothly.Data>) {
-		this.inputHandler(event)
 		if (!this.organization) {
 			const message = `${this.translate("No organization selected")}`
 			this.notice.emit(smoothly.Notice.failed(message))
-		} else if (!userwidgets.Organization.Changeable.Invite.is(event.detail)) {
+		} else if (!userwidgets.Organization.Changeable.Invite.is(event.detail.value)) {
 			const message = `${this.translate("Invalid form")}`
 			this.notice.emit(smoothly.Notice.failed(message))
 		} else {
-			const form = event.detail
-			this.disabled = true
-			const users = this.organization.users.includes(event.detail.user)
+			const form = event.detail.value
+			const users = this.organization.users.includes(event.detail.value.user)
 				? this.organization.users.map(email => (email != form.user ? email : { ...form }))
 				: [...this.organization.users, { ...form }]
 			const response = await this.state.organizations.update({ users }, { email: true })
@@ -55,23 +52,17 @@ export class UserwidgetsUserListInvite {
 				}`
 				this.notice.emit(smoothly.Notice.succeeded(message))
 			}
-			this.disabled = false
 		}
 	}
 
-	render() {
+	render(): VNode | VNode[] {
 		return (
 			<Host>
-				<smoothly-form
-					ref={e => (this.form = e)}
-					looks="grid"
-					processing={this.disabled}
-					onSmoothlyFormInput={e => this.inputHandler(e)}
-					onSmoothlyFormSubmit={e => this.submitHandler(e)}>
+				<smoothly-form ref={e => (this.form = e)} looks="grid" onSmoothlyFormSubmit={e => this.submitHandler(e)}>
 					<smoothly-input type="email" name={"user"} value={this.invite?.user}>
 						{this.translate("Email")}
 					</smoothly-input>
-					<userwidgets-edit-button slot="submit" state={this.state} disabled={this.disabled} toggle={false} />
+					<userwidgets-edit-button slot="submit" state={this.state} toggle={false} />
 				</smoothly-form>
 			</Host>
 		)
